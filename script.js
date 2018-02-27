a3018a73263254ae30a9fc6fa423775da561bc78
slaus <mister.slaus@gmail.com>
2018-02-27 22:56:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 jQuery(function($){
-    $(window).on("ready load scroll resize", function() {
-        
-        var sameColumn = 0;
-       
-        function equalHeight(group, browserWidth) {
-            if (browserWidth !== "") {
-                
-                group.each(function() {
-                    thisHeight = $(this).height();
-                    (thisHeight > sameColumn) ? sameColumn = thisHeight: "";
-                });
-                group.height(sameColumn);
-            } else {
-            
-            }
-        }
-        
-        function windowSize(group, browserWidth){
-            if ($(window).width() > browserWidth) {
-                group.each(function() {
-                    thisHeight = $(this).height();
-                    (thisHeight > sameColumn) ? sameColumn = thisHeight: "";
-                });
-                group.height(sameColumn);
-            }
-        }
 
-        equalHeight($(".post-item"), 767);
+  $(window).on("load ready scroll resize", function() {
+
+    var sameColumn = 0;
+       
+    function equalHeight(group,browserWidth) {
+      if (browserWidth !== "") {
+        if ($(window).width() > browserWidth) {
+                
+          group.each(function() {
+            thisHeight = $(this).height();
+            (thisHeight > sameColumn) ? sameColumn = thisHeight: "";
+          });
+        }
+      } else {
+        group.each(function() {
+          thisHeight = $(this).height();
+          (thisHeight > sameColumn) ? sameColumn = thisHeight: "";
+        });
+      }
+      group.height(sameColumn);
+    }
+
+    equalHeight($(".test"),900);
         
-    });
-    
+  });
 });
