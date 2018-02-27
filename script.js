ea39771dfa22665d810093cb9fb2bc91a3605b41
slaus <mister.slaus@gmail.com>
2018-02-27 22:00:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 jQuery(function($){
     $(window).on("ready load scroll resize", function() {
-
+        
+        var sameColumn = 0;
+       
         function equalHeight(group, browserWidth) {
             if (browserWidth !== "") {
-                var sameColumn = 0;
+                
+                group.each(function() {
+                    thisHeight = $(this).height();
+                    (thisHeight > sameColumn) ? sameColumn = thisHeight: "";
+                });
+                group.height(sameColumn);
+            } else {
+            
+            }
+        }
+        
+        function windowSize(group, browserWidth){
+            if ($(window).width() > browserWidth) {
                 group.each(function() {
                     thisHeight = $(this).height();
                     (thisHeight > sameColumn) ? sameColumn = thisHeight: "";
@@ -13,5 +27,7 @@ jQuery(function($){
         }
 
         equalHeight($(".post-item"), 767);
+        
     });
+    
 });
